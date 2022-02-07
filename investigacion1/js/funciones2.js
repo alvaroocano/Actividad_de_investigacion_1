b379d1545274f98534138ac82042468548dff188
alvaroocano <91907679+alvaroocano@users.noreply.github.com>
2022-02-07 09:38:06 +0100

    Add files via upload

---
+++ investigacion1/js/funciones2.js
@@ -0,0 +1,4 @@
+function cargarPagina()
+{
+	document.getElementsByTagName("body")[0].innerHTML="<p>Lorem salutandi eu mea, eam in soleat iriure assentior. Tamquam lobortis id qui. Ea sanctus democritum mei, per eu alterum electram adversarium. Ea vix probo dicta iuvaret, posse epicurei suavitate eam an, nam et vidit menandri. Ut his accusata petentium.</p>";
+}
